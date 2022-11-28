51fd79cc5c66354212460925820445e66ff90017
Camvan955 <nguyenvann955@gmail.com>
2022-11-28 16:37:51 +0700

    Nop bai tap ss7

--- ss7_spring_boot/exercise/src/main/resources/data.sql
+++ ss7_spring_boot/exercise/src/main/resources/data.sql
@@ -4,7 +4,7 @@ values (1, 'Tiểu thuyết'),
         (3, 'Tự do');
 insert into blog.blog(id, name, content, author, day_of_write, category_id)
 values (1, 'Dế mèn phiêu lưu kí', 'hành trình phiêu lưu của dế Mèn', 'Tô Hoài', '1991-12-12', 1),
-        (2, 'Java cơ bản', 'bjcgcubsjkcbsu', 'Nguyễn Văn A', '1999-01-21', 2),
+        (2, 'Java cơ bản', 'code java', 'Nguyễn Văn A', '1999-01-21', 2),
         (3, 'Quà tặng cuộc sống', 'Nhiều tác giả', 'cjhgycfácba', '2020-12-1', 3),
         (4, 'Trạng Quỳnh', 'Nhiều tác giả', 'cjhgycfácba', '2020-01-03', 3),
         (5, 'Hạt giống tâm thần', 'Nhiều tác giả', 'cjhgycfácba', '1999-01-08', 1),
@@ -12,7 +12,7 @@ values (1, 'Dế mèn phiêu lưu kí', 'hành trình phiêu lưu của dế Mè
         (7, 'Hành trình chữa lành', 'chữa lành tâm hồn', 'Trần Đình Thi', '2009-01-18', 2),
         (8, 'Hạt giống tâm thần', 'Nhiều tác giả', 'cjhgycfácba', '2020-12-19', 1),
         (19, 'Dế mèn phiêu lưu kí', 'hành trình phiêu lưu của dế Mèn', 'Tô Hoài', '1022-10-11', 1),
-        (10, 'Java cơ bản', 'bjcgcubsjkcbsu', 'Nguyễn Văn A', '2019-01-04', 2),
+        (10, 'Java cơ bản', 'code java', 'Nguyễn Văn A', '2019-01-04', 2),
         (11, 'Quà tặng cuộc sống', 'Nhiều tác giả', 'cjhgycfácba', '2019-01-02', 3),
         (12, 'Trạng Quỳnh', 'Nhiều tác giả', 'cjhgycfácba', '2022-11-12', 3),
         (13, 'Hạt giống tâm thần', 'Nhiều tác giả', 'cjhgycfácba', '2021-02-01', 1),
